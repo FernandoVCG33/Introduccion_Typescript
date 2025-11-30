2ab5ffd5b0f6dce2941af99e1dd0669287f638ec
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-11-30 12:11:49 -0400

    Actualización a Node 24 y arreglos iniciales

--- 01-TypeScript-intro/src/main.ts
+++ 01-TypeScript-intro/src/main.ts
@@ -1,9 +1,6 @@
-import './style.css'
-
+import './style.css'  //es un modulo 
+//import './tipocs/01-bases'
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML=`Hola mundo empezamos con Angular`;
 console.log("esta es mi primera app en Angular y aprendiendo TypeScript");
-for(let x= 0 ; x<=10  ;x++){
-  console.log(` el numero es: ${x}`);
-}
---
+++ 01-TypeScript-intro/src/tipocs/01-bases.ts
@@ -0,0 +1,8 @@
+const name : string = "Fernando Catacora" ;
+let hpPoints: number | 'FULL' = 45;
+hpPoints = 150 ;
+const isAlive : boolean = true ; 
+console.log({
+    name,hpPoints,isAlive
+});
+export{};
