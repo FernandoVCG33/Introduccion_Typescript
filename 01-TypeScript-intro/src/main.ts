43cb34d1f08c78bbdda9834d0746e349fa8482ed
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-01 14:19:48 -0400

    Se agrego todos los cambios

--- 01-TypeScript-intro/src/main.ts
+++ 01-TypeScript-intro/src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'  //es un modulo 
 //import './tipocs/01-bases'
-import'./tipocs/05-desestructuracion';
+import'./topics/05-desestructuracion';
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML=`Hola mundo empezamos con Angular`;
